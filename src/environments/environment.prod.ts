d1076ae70f480498e9a00956029b2d16872f4d35
Ravetzky Richard <richard.ravetzky@gmail.com>
2018-06-04 21:57:58 +0200

    Firebase full service init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyA2ZxVXOJMNo-khpKvExiSCItTqJVQw8cI",
+    authDomain: "test-47f4d.firebaseapp.com",
+    databaseURL: "https://test-47f4d.firebaseio.com",
+    projectId: "test-47f4d",
+    storageBucket: "test-47f4d.appspot.com",
+    messagingSenderId: "374222019354"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyA2ZxVXOJMNo-khpKvExiSCItTqJVQw8cI",
+    authDomain: "test-47f4d.firebaseapp.com",
+    databaseURL: "https://test-47f4d.firebaseio.com",
+    projectId: "test-47f4d",
+    storageBucket: "test-47f4d.appspot.com",
+    messagingSenderId: "374222019354"
+  }
+
 };
 
 /*
