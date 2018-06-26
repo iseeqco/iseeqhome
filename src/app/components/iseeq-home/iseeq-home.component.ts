fa9bc300a93899b5952076b352d8be93df94a3ae
Ravetzky Richard <richard.ravetzky@gmail.com>
2018-06-27 00:00:15 +0200

    must

--- src/app/components/iseeq-home/iseeq-home.component.ts
+++ src/app/components/iseeq-home/iseeq-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { IseeqNavigationService } from '../../services/iseeq-navigation.service';
 
@@ -13,9 +13,20 @@ export class IseeqHomeComponent{
     onScroll(event:any){
       if ((window.innerHeight + window.scrollY) >= this.element.nativeElement.firstElementChild.offsetHeight) {
          this.navService.sitePositionDatas=[];
-         //this.router.navigate(['/home/services'])
+         this.router.navigate(['/home/services'])
         }
     }
+
+@ViewChild('cat')
+    private catElement:ElementRef;
+@ViewChild('dog')
+    private dogElement:ElementRef;
+@ViewChild('coin')
+    private coinElement:ElementRef;
+@ViewChild('coin2')
+    private coin2Element:ElementRef;
+@ViewChild('coin3')
+    private coin3Element:ElementRef;
 
   constructor(
     private router:Router,
@@ -28,6 +39,43 @@ export class IseeqHomeComponent{
  ngOnInit(){
    for(let i=0;i<this.navService.componentRemote.length;i++){this.navService.componentRemote[i]=false}
    this.navService.areSitesOpen=false;
+  
   }
+
+ 
+
+spriteAnimation(picture:ElementRef,unit,limit:number,intervall:number,repeat ? :number):void{
+  let counter:number = 0;
+  let aktuelPosition = unit;
+
+  let InterV = setInterval(()=>{
+   
+  picture.nativeElement.style.backgroundPosition=`-${aktuelPosition}px 0px`
+
+  if (aktuelPosition < limit){aktuelPosition=aktuelPosition+unit} else {aktuelPosition=0}
+  counter ++
+  console.log(counter)
+  if(counter==repeat){clearInterval(InterV);console.log("clear")}
+
+},intervall)
+  
+}  
+animateCat():void{
+  this.spriteAnimation(this.catElement,45,45,500,60)
+  this.spriteAnimation(this.dogElement,45,45,500,60)
+  this.spriteAnimation(this.coinElement,20,40,230,120)
+  this.spriteAnimation(this.coin2Element,20,40,210,120)
+  this.spriteAnimation(this.coin3Element,20,40,250,120)
+
+  //this.catElement.nativeElement.style.backgroundPosition="-45px 0px"
+  
+  
+}
+animateCatBack(){
+  //this.spriteAnimation(this.catElement,45,45)
+ // this.spriteAnimation(this.dogElement,45,45)
+  //console.log("back")
+  //this.catElement.nativeElement.style.backgroundPosition="0px 0px"
+}
 
 }
