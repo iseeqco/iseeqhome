65d4d9b03a5f3d88cbd7038874f0e94acbaa2646
R80Richard <richard.ravetzky@gmail.com>
2018-07-09 16:07:19 +0100

    Team component 0.2

--- src/app/ContetntFeature/components/iseeq-team/iseeq-flipcard.component.ts
+++ src/app/ContetntFeature/components/iseeq-team/iseeq-flipcard.component.ts
@@ -1,7 +1,6 @@
 import {Input,Component} from '@angular/core';
 import {TeamMember } from '../../../datatypes/iseeq-team-member';
 
-
 @Component({
     selector:'flip-card-component',
     templateUrl:'iseeq-flip-card.component.html',
@@ -12,7 +11,9 @@ export class IseeqFlipCardComponent {
              animTrigger:string;
              frontContentVisibility:string;
              backContentVisibility:string;
+             svgString:string='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" height="25" width="25" fill="#45d48b"><path d="M32.79 8.334h5.3V0h-7.945s-4.994-.123-8.178 4.014c0 0-2.17 2.046-2.204 8.032v6.25H11.91v8.847h7.853V50h9.055V27.145h7.79l1.088-8.848h-8.878v-6.25h-.002c.022-.698.335-3.776 3.973-3.713z"/></svg>'  
    
+
     constructor()
     {
         this.animTrigger='card'
@@ -20,8 +21,11 @@ export class IseeqFlipCardComponent {
         this.backContentVisibility='hidden'
     }
     
+    ngAfterViewInit(){
+    
+    }
     ngOnInit(){
-   
+        
     }
 
     onmouseenter(){
--- src/app/datatypes/iseeq-team-member.ts
+++ src/app/datatypes/iseeq-team-member.ts
@@ -3,6 +3,7 @@ import { SocialMediaLink } from "./iseeq-footer-config.data";
 
 export class TeamMember {
     profilePicture:string;
+    pictureAlt:string;
     name:string;
     position:string;
     contacts:SocialMediaLink[];
